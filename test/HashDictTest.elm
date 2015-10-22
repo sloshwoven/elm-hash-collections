dc4b2f993c07169aabbc6d1f5dd9bc8c4bd9ee10
sloshwoven <slosh.woven@gmail.com>
2015-10-21 22:01:25 -0400

    Add skeleton suites for HashDict function groups

--- test/HashDictTest.elm
+++ test/HashDictTest.elm
@@ -4,4 +4,30 @@ import Check as C
 
 hashDictSuite : C.Claim
 hashDictSuite =
-    C.suite "HashDict suite" []
+    [ buildSuite
+    , querySuite
+    , combineSuite
+    , listsSuite
+    , transformSuite
+    ]
+    |> C.suite "HashDict suite"
+
+buildSuite : C.Claim
+buildSuite =
+    C.suite "build suite" []
+
+querySuite : C.Claim
+querySuite =
+    C.suite "query suite" []
+
+combineSuite : C.Claim
+combineSuite =
+    C.suite "combine suite" []
+
+listsSuite : C.Claim
+listsSuite =
+    C.suite "lists suite" []
+
+transformSuite : C.Claim
+transformSuite =
+    C.suite "transform suite" []
