a774c51f0d2725679895f05a3cb62aa955c278d2
sloshwoven <slosh.woven@gmail.com>
2016-03-01 19:12:39 -0500

    Add placeholder for HashDict unit (not quick) tests

--- test/AllTest.elm
+++ test/AllTest.elm
@@ -2,15 +2,23 @@ import Check as C
 import Check.Test as T
 import ElmTest as ET
 import Graphics.Element as GE
-import HashDictTest exposing (..)
-import HashSetTest exposing (..)
+import HashDictTest as HDQC
+import HashDictUnitTest as HDU
+import HashSetTest as HSQC
 
 main : GE.Element
 main =
-    [ hashDictSuite
-    , hashSetSuite
-    ]
-    |> C.suite "elm-hash-collections"
-    |> C.quickCheck
-    |> T.evidenceToTest
-    |> ET.elementRunner
+    let
+        qcHashDictTest =
+            C.quickCheck HDQC.hashDictSuite
+            |> T.evidenceToTest
+        qcHashSetTest =
+            C.quickCheck HSQC.hashSetSuite
+            |> T.evidenceToTest
+    in
+        ET.suite "elm-hash-collections"
+            [ qcHashDictTest
+            , qcHashSetTest
+            , HDU.hashDictUnitSuite
+            ]
+        |> ET.elementRunner
---
+++ test/HashDictUnitTest.elm
@@ -0,0 +1,8 @@
+module HashDictUnitTest ( hashDictUnitSuite) where
+
+import ElmTest as ET
+
+hashDictUnitSuite : ET.Test
+hashDictUnitSuite =
+    ET.suite "HashDict unit tests"
+        []
